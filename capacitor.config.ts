c8e41233dc6a67e065e4f22e9da6ddff2151589a
Bochaxd <andresloa.al@gmail.com>
2022-11-29 22:04:42 -0500

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'tabs3',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
